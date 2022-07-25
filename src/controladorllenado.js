09a70263242de69cbd6234f99f93081f615c1875
David Molina <davidmolinatorres789@gmail.com>
2022-07-25 11:17:22 -0500

    tienda

---
+++ src/controladorllenado.js
@@ -0,0 +1,148 @@
+let productosBD= [
+    {
+        nombre:"aguardiente antioqueño",
+        precio:55000,
+        cantidad:0,
+        foto:['https://firebasestorage.googleapis.com/v0/b/molinatiendatcc.appspot.com/o/guaro.webp?alt=media&token=b2290639-a3be-4c3a-9c7e-2667a0dc39e0','https://firebasestorage.googleapis.com/v0/b/molinatiendatcc.appspot.com/o/guaro.webp?alt=media&token=b2290639-a3be-4c3a-9c7e-2667a0dc39e0',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWiI32oaWqBudbBAgcFkveRHzlfVuTwoJ34w&usqp=CAU"],
+        descripcion:'aguardiente antioqueño, el aguardiente de la gente '
+    },
+    {
+       
+        
+        nombre:'Ron Medellin',
+        precio:45000,
+        cantidad:6,
+        foto:['https://d2j6dbq0eux0bg.cloudfront.net/images/30491376/1511186860.jpg','http://www.licoresfederal.com/web/image/product.template/192/image'],
+        descripcion:'el ron  mas fuerte'
+     },
+    {
+        nombre:'ron caldas',
+        precio:48000,
+        cantidad:4,
+        foto:['https://olimpica.vtexassets.com/arquivos/ids/606895/7702168218305.jpg?v=637626493136270000','https://surtilicor.com/wp-content/uploads/2020/10/RON-VIEJO-DE-CALDAS-TRADICIONAL-BOTELLA-750-ML.jpg'],
+       descripcion:'ron añejo de mejor sabor '
+    },
+    
+    {
+         nombre:' cerveza smirnoff',
+        precio:8000,
+        cantidad:18,
+        foto:['https://restaurante.231e.com/wp-content/uploads/2020/12/producto-cervesa-smirnoff.png','image.png'],
+        descripcion:'cerveza de vodka'
+    },
+    {
+        nombre:'heinenken',
+        precio:9000,
+        cantidad:20,
+        foto:['https://koyotebarbacoa.com/wp-content/uploads/2020/04/heineken-01.png','https://flyclipart.com/thumbs/heineken-front-label-heineken-beer-1582414.png'],
+        descripcion:'cerveza elaborada en amsterdan '
+    },
+        
+     
+    {
+        nombre:'corona',
+        precio:7000,
+        cantidad:28,
+        foto:['https://distrilicoresdeltolima.com/wp-content/uploads/2020/06/589-5897300_corona-transparent-710ml-botella-de-corona-hd-png.png','https://olimpica.vtexassets.com/arquivos/ids/821530/2126117-1.jpg?v=637872126962130000'],
+        descripcion:'cerza elaborada abase de arroz'
+    },
+    {
+        nombre:'aguila',
+        precio:3000,
+        cantidad:30,
+        foto:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtvknTlRhSSV3t2Srz3H26SbBkNC6E2DZesg&usqp=CAU','https://www.cervezaaguila.com/sites/g/files/yrakuj311/files/2022-02/aguila-original.png'],
+        descripcion:'cerveza original colombiana '
+    },
+    {
+        nombre:'pilsen',
+        precio:3000,
+        cantidad:40,
+        foto:['https://www.bavaria.co/sites/g/files/phfypu1316/f/IMG-pilsen-2.jpg','https://www.bavaria.co/sites/g/files/phfypu1316/f/pilsen-botella.png'],
+        descripcion:'cerveza tradicional colombiana elaborada por bavaria'
+    },
+    {
+        nombre:'stella',
+        precio:11000,
+        cantidad:14,
+        foto:['https://restaurantetortelli.com/wp-content/uploads/2021/08/STELLA_ARTOIS.jpg','https://static.turbosquid.com/Preview/2017/10/09__17_09_25/pic_00000.jpgFF4A77C8-C0B7-47FA-9C40-539B38900930Large.jpg'],
+        descripcion:'cerveza de origen belga'
+    },
+    {
+        nombre:'jagermeister',
+        precio:65000,
+        cantidad:12,
+        foto:['https://www.eltiempo.com/uploads/2021/01/09/5ffa7ea8ea173.jpeg','https://www.cocinayvino.com/wp-content/uploads/2017/08/jagerstaimer-1200x675.jpg'],
+        descripcion:'es de los pocos whisky con un sabor dulce'
+    },
+    {
+        nombre:'1800',
+        precio:80000,
+        cantidad:16,
+        foto:['https://www.lallardelvi.com/2913-tm_thickbox_default/tequila-1800-reposado-.jpg','https://i0.wp.com/shakeadito.com/wp-content/uploads/2021/04/POSTS-TEQUILA-1800-PORTADA.jpg?fit=1370%2C929&ssl=1'],
+        descripcion:'Es un tequila de origen mexicano'
+    },
+       
+    {
+        nombre:'buchanans',
+        precio:130000,
+        cantidad:28,
+        foto:['https://revistaelconocedor.com/wp-content/uploads/2019/09/Buch-1024x683.jpg','https://m.media-amazon.com/images/I/71daA1BQmFL._AC_SX342_.jpg'],
+        descripcion:'Whisky de origen irlandes '
+    },
+    {
+        nombre:'old parr',
+        precio:120000,
+        cantidad:23,
+        foto:['https://cdn.shopify.com/s/files/1/0335/3663/9116/products/unnamed_8_2491af3b-52d0-486e-b7f5-1c6540d70561_800x.jpg?v=1607175674'],
+        descripcion:'Whisky de origen Alberbury, Reino Unido'
+    },
+     
+    
+    {
+        nombre:'red bull',
+        precio:11000,
+        cantidad:30,
+        foto:['https://media.istockphoto.com/photos/aluminium-can-of-red-bull-energy-drink-iced-background-picture-id537022544?k=20&m=537022544&s=612x612&w=0&h=r5OPbtcVPAxBkmnkyfczQwxfbFH9beInZeea0xxjoiM='],
+        descripcion:'Es un energizante fundado en los años 1980'
+    },
+    {
+        nombre:'mosnter',
+        precio:10000,
+        cantidad:40,
+        foto:['https://i.pinimg.com/originals/d4/65/6a/d4656abf5f9a2a55afa45e8604aea3d8.jpg','image.png'],
+        descripcion:'Es un energizante fundado en los años 1935'
+    }
+    
+]
+
+
+let fila=document.getElementById("fila")
+
+productosBD.forEach(function(producto){
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card", "h-100","w-100" ,"text-center")
+
+    let fotoProducto=document.createElement("img")
+    fotoProducto.classList.add ("img-fluid","w-100","h-100")
+    fotoProducto.src=producto.foto[0]
+
+    let nombreProducto=document.createElement("h3")
+    nombreProducto.textContent=producto.nombre
+
+    let precioProducto=document.createElement("p")
+    precioProducto.textContent='$'+producto.precio+'COP'
+
+    let DescripcionProducto=document.createElement("p")
+    DescripcionProducto.textContent=producto.descripcion
+
+    tarjeta.appendChild(fotoProducto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(DescripcionProducto)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+})
